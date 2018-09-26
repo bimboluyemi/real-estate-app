0c469021d9a8455816e0ae91bffef6787e154c2e
Abimbola Oluyemi <abimbolasamueloluyemi@gmail.com>
2018-09-26 16:46:16 -0400

    basic template

--- real_estate_app/settings.py
+++ real_estate_app/settings.py
@@ -55,7 +55,8 @@ ROOT_URLCONF = 'real_estate_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+                os.path.join(BASE_DIR, 'templates/home')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -120,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/static/',
+]
