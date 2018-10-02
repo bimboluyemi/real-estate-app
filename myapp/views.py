33450a17d33d456df8e11ef5ef6163263f1100a2
Abimbola Oluyemi <abimbolasamueloluyemi@gmail.com>
2018-10-02 17:35:23 -0400

    Simple Template Added

--- myapp/views.py
+++ myapp/views.py
@@ -6,3 +6,26 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
+
+def detail(request):
+    return render(request, 'detail.html')
+
+
+def search(request):
+    return render(request, 'search.html')
+
+
+def advertise(request):
+    return render(request, 'advertise.html')
+
+
+def signin(request):
+    return render(request, 'signin.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def about(request):
+    return render(request, 'about.html')
--- real_estate_app/urls.py
+++ real_estate_app/urls.py
@@ -22,4 +22,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', views.home, name='home'),
+    path('search/', views.search, name='search'),
+    path('advertise/', views.advertise, name='advertise'),
+    path('signin/', views.signin, name='signin'),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
+    path('detail/', views.detail, name='detail'),
+
 ]
