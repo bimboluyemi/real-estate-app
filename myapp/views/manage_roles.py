41c916ad90875d8b0dfc12653a53643aa778cf81
arthikdaniel <arthik.daniel@gmail.com>
2018-11-15 21:58:53 -0500

    Fix Navigation for Roles

--- myapp/views/manage_roles.py
+++ myapp/views/manage_roles.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from ..models import Role
 from ..forms.access_control_forms import RoleForm
 
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+
 
 def all_roles(request):
     return render(request, 'manage_roles/list.html',
@@ -27,3 +29,9 @@ def role(request, role_id=None):
 
 def delete_role(request):
     return 200
+
+
+class RoleDelete(DeleteView):
+    model = Role
+    template_name = 'manage_roles/delete.html'
+    success_url = '/system-admin/roles'
